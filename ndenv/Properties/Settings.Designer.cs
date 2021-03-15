5effb38ffa42b8de4e2373470968ab2e8ba91d79
enumori <@enumori>
2021-03-15 21:43:21 +0900

    LTSのバージョンをv14に変更

--- ndenv/Properties/Settings.Designer.cs
+++ ndenv/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace ndenv.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.5.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace ndenv.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("v12")]
+        [global::System.Configuration.DefaultSettingValueAttribute("v14")]
         public string LTSVersion {
             get {
                 return ((string)(this["LTSVersion"]));
